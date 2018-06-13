6fbd24b075ea5702510347d26e1c3a46c742913c
SixWhiteSwans <anthony@jetlifestyle.com>
2018-06-13 11:40:39 +0100

    R code

---
+++ SecuritiesViewerR/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RDotNet;
+
+namespace SecuritiesViewerR
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using (REngine engine = REngine.GetInstance())
+			{
+				engine.Initialize();
+
+				var result = EvaluateExpression(engine, "5+2+9");
+
+				Console.Write(result);
+				Console.ReadKey();
+			}
+
+		}
+
+		private static double EvaluateExpression(REngine engine, string expression)
+		{
+			var expressionVector = engine.CreateCharacterVector(new[] { expression });
+			engine.SetSymbol("expr", expressionVector);
+
+			//  WRONG -- Need to parse to expression before evaluation
+			//var result = engine.Evaluate("eval(expr)");
+
+			//  RIGHT way to do this!!!
+			var result = engine.Evaluate("eval(parse(text=expr))");
+			var ret = result.AsNumeric().First();
+
+			return ret;
+		}
+	}
+}
